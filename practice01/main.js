b16343ac707bc4733269e80e1849c1c46d73f481
jas-dev <jaysamora@outlook.com>
2019-02-17 18:50:56 -0800

    finished practice01

---
+++ practice01/main.js
@@ -0,0 +1,62 @@
+//cycle through array
+//look at the length of the string in each position of the array
+//compare the length of the string in each position of the array to the length  of the string passed in via the string parameter
+//if the string in the current array position is shorter than the string in the string argument, output (append?) to an array, else ignore it and check the next position of the string array
+//when the end of the string array is reached, return the value of the output array
+
+var gameArray=[
+    'Player Unknown\'s Battlegrounds',
+    'Apex',
+    'Battlefield V',
+    'Path of Exile',
+    'Hotline Miami',
+    'Smash',
+    'Quake',
+    'Fallout'
+];
+
+console.log("Games Library: "+gameArray);
+
+var nameOutputArray= [];
+
+function longNames(string,array){
+
+    //debugger;
+
+
+    for (var game= 0; game < array.length; game++){
+
+        if(string.length < array[game].length){
+            nameOutputArray.push(array[game].valueOf());
+            game++;
+
+        }else{
+            break;
+        }
+    }
+
+
+
+
+    return nameOutputArray;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
