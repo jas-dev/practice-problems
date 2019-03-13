6aff217c7493a4d993709d4ff4231815dafec275
jas-dev <jaysamora@outlook.com>
2019-03-13 12:55:42 -0700

    01 02 06 solved

---
+++ practice02/main.js
@@ -0,0 +1,29 @@
+
+
+
+var memeArray= [
+    'Has',
+    'Anyone',
+    'Really',
+    'Been',
+    'Far',
+    'Even',
+    'As',
+    'Decided',
+    'To Use',
+    'Even',
+    'Go',
+    'Want',
+    'To Do',
+    'Look',
+    'More',
+    'Like?'
+
+];
+
+console.log("Here's the array: "+memeArray);
+
+function reverseMeme(array){
+    array.reverse();
+    return array;
+}
